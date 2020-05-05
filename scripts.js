cd5ebe21a91297ae4d00933690c9cc14fa15e9e4
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-05-05 09:31:17 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$("#customerName").text("Simmy");
+$("#policyNumber").text("LM96219685");
+$("#startDate").text("05-May-2020");
+$("#endDate").text("04-May-2021");
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08CJ0536");
+$("#make").text("SUZUKI");
+$("#model").text("ACCESS");
+$("#vehicalSubType").text("SE(125CC)");
+$("#engineNo").text("F4861004105");
+$("#chassis").text("MB8CF4CAAD8311365");
